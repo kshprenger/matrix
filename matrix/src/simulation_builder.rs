f348ae91a0a0bb178c108c7d34c1e0cdc404498c
kshprenger <kostya@shprenger.com>
2026-01-04 16:09:12 +0100

    Typo

--- matrix/src/simulation_builder.rs
+++ matrix/src/simulation_builder.rs
@@ -9,7 +9,7 @@ use crate::{
     tso,
 };
 
-// There is a lot of Rc small allocations, so we optimize this too using different allocator
+// There are a lot of Rc small allocations, so we optimize this too using different allocator
 #[global_allocator]
 static GLOBAL_ALLOCATOR: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
