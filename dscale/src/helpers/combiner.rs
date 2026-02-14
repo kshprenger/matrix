d62dfae9d8cc00a3516eb64f1c01bf95e9b56452
kshprenger <kostya@shprenger.com>
2026-02-14 16:59:33 +0100

    docs

--- dscale/src/helpers/combiner.rs
+++ dscale/src/helpers/combiner.rs
@@ -1,6 +1,6 @@
 use std::usize;
 
-/// Multiple value gathering (fors example for quorums).
+/// Multiple values gathering (for example for quorums).
 /// Combiner size is passed in compile time so compiler can place quorum on the stack.
 pub struct Combiner<T: Sized, const K: usize> {
     quorum: [Option<T>; K],
